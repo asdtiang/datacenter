cdc53515a349d1cfadc47aa5984bd022ad470ccb
asdtiangxia@163.com <asdtiangxia@163.com>
2013-05-16 11:21:06 +0800

    entity数据库映射类建立好，更改数据库连接信息，发布到tomcat，表会自动建立

--- src/main/java/cn/longkai/struts/repository/UserDao.java
+++
@@ -1,9 +0,0 @@
-package cn.longkai.struts.repository;
-
-import cn.longkai.struts.entity.User;
-
-public interface UserDao {
-
-	User persist(User user);
-	
-}
--- src/main/java/cn/longkai/struts/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package cn.longkai.struts.service;
-
-import cn.longkai.struts.entity.User;
-
-public interface UserService {
-	
-	void save(User user);
-	
-}
